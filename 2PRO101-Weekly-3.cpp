ee6294890a0f6c395acfeda3084142df9b51677f
Xephoney <hans_olahoftun@hotmail.com>
2020-09-16 21:26:48 +0200

    First Alpha of the project. This has a random number generation, a difficulty selector and a Main menu. Although there is no 2 player mode yet. soon though.

--- 2PRO101-Weekly-3.cpp
+++ 2PRO101-Weekly-3.cpp
@@ -3,9 +3,8 @@
 #include <Windows.h>
 using namespace std;
 
-void PlayerVsComputer();
-void PlayerVsPlayer();
-
+int DifficultySelect();
+void Game(int difficulty, bool twoPlayer);
 int main()
 {
     //This code is to maximize the window on startup! 
@@ -25,10 +24,10 @@ int main()
 
     cout << " Welcome to Guess The Number! \n";
     int gamemode = 1;
-    
+    int _diffy = 10;
     do
     {
-        system("CLS");
+        //system("CLS");
         cout << " What would you like to do? \n";
         cout << "   [1] Play against the Computer \n";
         cout << "   [2] Player VS Player \n \n";
@@ -40,29 +39,83 @@ int main()
         case 0 :
             cout << "Bye Bye!";
             return 0;
-        case 1 : 
-            Sleep(2000);
-            PlayerVsComputer();
+        case 1 :
+            _diffy = DifficultySelect();
+            Game(_diffy, false);
             break;
         case 2 : 
-            cout << "Okay! PVP it is! ;) Bring your A-game!\n";
-            Sleep(2000);
-            PlayerVsPlayer();
+            _diffy = DifficultySelect();
+            Game(_diffy, true);
             break;
         default:
             cout << "Sorry, that is not a vaild answer. Try again! :) \n";
-            Sleep(1500);
             break;
         }
     } while (gamemode >= 0 || gamemode < 3);
 }
-
-void PlayerVsComputer()
+int DifficultySelect()
 {
-
-
+    int input = 0;
+    do
+    {
+        std::cout << "Select your difficulty : \n";
+        std::cout << "[1] Easy (1 - 5) \n" << "[2] Medium (1 - 10) \n" << "[3] Hard (1 - 30) \n" << "How good do you think you are? : ";
+        std::cin >> input;
+        
+        switch (input)
+        {
+        case 1 : 
+            std::cout << "You selected Easy \n";
+            return 5;
+            break;
+        case 2 :
+            std::cout << "You selected Medium \n";
+            return 10;
+            break;
+        case 3:
+            std::cout << "You selected Hard! Oooo, i hope you fell lucky! \n";
+            return 30;
+            break;
+        default:
+            std::cout << "Sorry, that is not a valid input! Please retry! \n";
+            break;
+        }
+    } while (input > 0 && input < 4);
+    
+    return 0;
 }
-void PlayerVsPlayer()
+void Game(int difficulty, bool twoPlayer)
 {
+    bool gameOn = true;
+    int guess;
+    int totalGuesses;
+    int computerNumber = rand() % difficulty + 1;
+    std::cout << "FOR DEBUGGIN PURPOSES : computer number is " << computerNumber << std::endl << std::endl << std::endl;
+    if (twoPlayer == false)
+    {
+        std::cout << "Alright Lets begin! The computer has now selected a number between 1 and " << difficulty << "! \n";
+        std::cout << "What is your guess? : ";
+        totalGuesses = 0;
+        while (gameOn) {
 
+            std::cin >> guess;
+            totalGuesses++;
+            if (guess > computerNumber)
+            {
+                std::cout << "Lower \n";
+            }
+            else if (guess < computerNumber)
+            {
+                std::cout << "Higher \n";
+            }
+            else if (guess == computerNumber)
+            {
+                std::cout << "Yes! That is correct!\n";
+                gameOn = false;
+            }
+        }
+        std::cout << "Great Work! That only took " << totalGuesses << " tries!";
+        system("pause");
+        return;
+    }
 }
