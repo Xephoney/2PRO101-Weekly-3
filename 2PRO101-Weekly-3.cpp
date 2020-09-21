280c42166140f45b90cbd104787f5b036436bb02
Hans Ola Hoftun <hans_olahoftun@hotmail.com>
2020-09-21 20:38:33 +0200

    Final Upload before handing in. This has been tested and tuned down so its not as complicated

--- 2PRO101-Weekly-3.cpp
+++ 2PRO101-Weekly-3.cpp
@@ -1,121 +1,105 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
+#include <time.h>
+
 using namespace std;
 
-int DifficultySelect();
-void Game(int difficulty, bool twoPlayer);
+void PlayerVsComputer();
+
+int difficulty;
+int currentHighscore = 5000;
 int main()
 {
     //This code is to maximize the window on startup! 
     //Taken from : https://stackoverflow.com/questions/6606884/setting-console-to-maximized-in-dev-c 
         HWND hWnd = GetConsoleWindow();
         ShowWindow(hWnd, SW_SHOWMAXIMIZED);
-
+        
+        std::srand((unsigned)time(0));
     
-    cout << "    _______  __   __  _______  _______  _______    _______  __   __  _______      __    _  __   __  __   __  _______  _______  ______    __  " << endl;
-    cout << "   |       ||  | |  ||       ||       ||       |  |       ||  | |  ||       |    |  |  | ||  | |  ||  |_|  ||  _    ||       ||    _ |  |  | " << endl;
-    cout << "   |    ___||  | |  ||    ___||  _____||  _____|  |_     _||  |_|  ||    ___|    |   |_| ||  | |  ||       || |_|   ||    ___||   | ||  |  | " << endl;
-    cout << "   |   | __ |  |_|  ||   |___ | |_____ | |_____     |   |  |       ||   |___     |       ||  |_|  ||       ||       ||   |___ |   |_||_ |  | " << endl;
-    cout << "   |   ||  ||       ||    ___||_____  ||_____  |    |   |  |       ||    ___|    |  _    ||       ||       ||  _   | |    ___||    __  ||__| " << endl;
-    cout << "   |   |_| ||       ||   |___  _____| | _____| |    |   |  |   _   ||   |___     | | |   ||       || ||_|| || |_|   ||   |___ |   |  | | __  " << endl;
-    cout << "   |_______||_______||_______||_______||_______|    |___|  |__| |__||_______|    |_|  |__||_______||_|   |_||_______||_______||___|  |_||__|" << endl;
-    cout << endl << endl;
-
-    cout << " Welcome to Guess The Number! \n";
-    int gamemode = 1;
-    int _diffy = 10;
+        int gamemode = 1;
+    
     do
     {
-        //system("CLS");
-        cout << " What would you like to do? \n";
-        cout << "   [1] Play against the Computer \n";
-        cout << "   [2] Player VS Player \n \n";
+        cout << "    _______  __   __  _______  _______  _______    _______  __   __  _______      __    _  __   __  __   __  _______  _______  ______    __  " << endl;
+        cout << "   |       ||  | |  ||       ||       ||       |  |       ||  | |  ||       |    |  |  | ||  | |  ||  |_|  ||  _    ||       ||    _ |  |  | " << endl;
+        cout << "   |    ___||  | |  ||    ___||  _____||  _____|  |_     _||  |_|  ||    ___|    |   |_| ||  | |  ||       || |_|   ||    ___||   | ||  |  | " << endl;
+        cout << "   |   | __ |  |_|  ||   |___ | |_____ | |_____     |   |  |       ||   |___     |       ||  |_|  ||       ||       ||   |___ |   |_||_ |  | " << endl;
+        cout << "   |   ||  ||       ||    ___||_____  ||_____  |    |   |  |       ||    ___|    |  _    ||       ||       ||  _   | |    ___||    __  ||__| " << endl;
+        cout << "   |   |_| ||       ||   |___  _____| | _____| |    |   |  |   _   ||   |___     | | |   ||       || ||_|| || |_|   ||   |___ |   |  | | __  " << endl;
+        cout << "   |_______||_______||_______||_______||_______|    |___|  |__| |__||_______|    |_|  |__||_______||_|   |_||_______||_______||___|  |_||__|" << endl;
+        cout << endl << endl;
+
+        cout << " Welcome to Guess The Number! \n";
+        
+        cout << " How lucky do you feel? \n";
+        cout << "   [1] Easy (1 - 5) \n";
+        cout << "   [2] Medium (1 - 10) \n";
+        cout << "   [3] Hard (1 - 20) \n";
+        cout << "   [4] Extreme (1 - 50) \n\n";
         cout << "   [0] Exit \n";
         cin >> gamemode;
 
         switch (gamemode)
         {
-        case 0 :
-            cout << "Bye Bye!";
-            return 0;
-        case 1 :
-            _diffy = DifficultySelect();
-            Game(_diffy, false);
+        case 1 : 
+            difficulty = 5;
+            PlayerVsComputer();
             break;
         case 2 : 
-            _diffy = DifficultySelect();
-            Game(_diffy, true);
-            break;
-        default:
-            cout << "Sorry, that is not a vaild answer. Try again! :) \n";
-            break;
-        }
-    } while (gamemode >= 0 || gamemode < 3);
-}
-int DifficultySelect()
-{
-    int input = 0;
-    do
-    {
-        std::cout << "Select your difficulty : \n";
-        std::cout << "[1] Easy (1 - 5) \n" << "[2] Medium (1 - 10) \n" << "[3] Hard (1 - 30) \n" << "How good do you think you are? : ";
-        std::cin >> input;
-        
-        switch (input)
-        {
-        case 1 : 
-            std::cout << "You selected Easy \n";
-            return 5;
-            break;
-        case 2 :
-            std::cout << "You selected Medium \n";
-            return 10;
+            difficulty = 10;
+            PlayerVsComputer();
             break;
         case 3:
-            std::cout << "You selected Hard! Oooo, i hope you fell lucky! \n";
-            return 30;
+            difficulty = 20;
+            PlayerVsComputer();
+            break;
+        case 4:
+            difficulty = 50;
+            PlayerVsComputer();
             break;
         default:
-            std::cout << "Sorry, that is not a valid input! Please retry! \n";
+            std::cout << "Well Bye! ";
             break;
         }
-    } while (input > 0 && input < 4);
-    
-    return 0;
-}
-void Game(int difficulty, bool twoPlayer)
-{
-    bool gameOn = true;
-    int guess;
-    int totalGuesses;
-    int computerNumber = rand() % difficulty + 1;
-    std::cout << "FOR DEBUGGIN PURPOSES : computer number is " << computerNumber << std::endl << std::endl << std::endl;
-    if (twoPlayer == false)
-    {
-        std::cout << "Alright Lets begin! The computer has now selected a number between 1 and " << difficulty << "! \n";
-        std::cout << "What is your guess? : ";
-        totalGuesses = 0;
-        while (gameOn) {
+        system("CLS");
+    } while (gamemode > 0 && gamemode < 5);
 
-            std::cin >> guess;
-            totalGuesses++;
-            if (guess > computerNumber)
-            {
-                std::cout << "Lower \n";
-            }
-            else if (guess < computerNumber)
-            {
-                std::cout << "Higher \n";
-            }
-            else if (guess == computerNumber)
-            {
-                std::cout << "Yes! That is correct!\n";
-                gameOn = false;
-            }
-        }
-        std::cout << "Great Work! That only took " << totalGuesses << " tries!";
-        system("pause");
-        return;
-    }
+
 }
+void PlayerVsComputer()
+{
+    int guess = 0;
+    int guesses = 0;
+    int randomNR = std::rand() % difficulty + 1;
+    std::cout << "Dbug random number = " << randomNR << "\n";
+    //std::cout << "Select a number between 1 - " << randomNR << " : ";
+
+    do
+    {
+        std::cout << "What's your guess? \n";
+        std::cin >> guess;
+
+        guesses++;
+        if (guess < randomNR)
+        {
+            std::cout << "Higher... \n";
+        }
+        else if (guess > randomNR)
+        {
+            std::cout << "Lower... \n";
+        }
+        else if (guess == randomNR)
+        {
+            std::cout << "Eyy! You did it! \n";
+        }
+    } while (guess != randomNR);
+
+    if (guesses < currentHighscore)
+    {
+        std::cout << "Congratulations! You get the new highscore of " << guesses << " tries! Well played Sir! \n\n";
+    }
+    system("pause");
+}
+
